dc2b82092818140620bb6ad1d144cbaff2d4c9ec
Serhii Belous <serhii.belous030@gmail.com>
2025-08-31 19:35:52 +0200

    chore: add user_agents.py to utils

---
+++ corex/utils/user_agents.py
@@ -0,0 +1,9 @@
+import random
+
+def get_useragent():
+    """PyPI: googlesearch-python"""
+    lynx_version = f"Lynx/{random.randint(2, 3)}.{random.randint(8, 9)}.{random.randint(0, 2)}"
+    libwww_version = f"libwww-FM/{random.randint(2, 3)}.{random.randint(13, 15)}"
+    ssl_mm_version = f"SSL-MM/{random.randint(1, 2)}.{random.randint(3, 5)}"
+    openssl_version = f"OpenSSL/{random.randint(1, 3)}.{random.randint(0, 4)}.{random.randint(0, 9)}"
+    return f"{lynx_version} {libwww_version} {ssl_mm_version} {openssl_version}"
