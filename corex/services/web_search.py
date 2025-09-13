2e5280197f724e53068d3fa91c216f0a77d64228
Serhii Belous <serhii.belous030@gmail.com>
2025-09-13 16:52:19 +0200

    feat: replace to DDG search engine

--- corex/services/web_search.py
+++ corex/services/web_search.py
@@ -1,49 +1,40 @@
 from requests import get
 from bs4 import BeautifulSoup
 from ..utils import get_useragent
-from urllib.parse import unquote
 
 
 def _request(query):
     response = get(
-        url="https://google.com/search",
+        url="https://duckduckgo.com/html",
         params={
-            "q": query,
-            "hl": "en"
+            "q": query
         },
         headers={
             "User-Agent": get_useragent(),
             "Accept": "*/*"
-        },
-        cookies={
-            'CONSENT': 'PENDING+987',
-            'SOCS': 'CAESHAgBEhIaAB'
         }
     )
     response.raise_for_status()
     return response
 
 
-def search(query: str) -> list[dict[str]]:
+def search(query: str) -> list[dict[str, str]]:
     response = _request(query)
     results = []
 
     soup = BeautifulSoup(response.text, "html.parser")
-    all_links = soup.find_all("div", class_="ezO2md")
+    all_links = soup.find_all("div", class_="links_main")
 
     for link in all_links:
-        title_tag = link.find("span", class_="CVA68e")
-        url_tag = link.find("a", href=True)
-        description_tag = link.find("span", class_="FrIlee")
-
-        url = unquote(url_tag["href"].split("&")[0].replace("/url?q=", "")) if url_tag else None
-        if url and not url.startswith("https://"):
-            continue
-
+        title_tag = link.find("a", class_="result__a")
+        url_tag = link.find("a", class_="result__url")
+        snippet_tag = link.find("a", class_="result__snippet")
+        
         title = title_tag.text if title_tag else None
-        description = description_tag.text.strip() if description_tag else None
-
-        if title and url and description:
-            results.append({"title": title, "url": url, "description": description})
+        url = url_tag.text.strip() if url_tag else None
+        snippet = snippet_tag.text if snippet_tag else None
+        
+        if title and url and snippet:
+            results.append({"title": title, "url": url, "snippet": snippet})
 
     return results
