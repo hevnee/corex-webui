315b1a56314be46a97d50554cb42e7e1f96df5be
Serhii Belous <serhii.belous030@gmail.com>
2025-09-13 16:48:37 +0200

    feat: add try handler

--- corex/services/generator.py
+++ corex/services/generator.py
@@ -11,37 +11,40 @@ async def text_generation(
     web_search: bool = False,
     agent: bool = False
 ):
-    client = AsyncClient()
-    message = chat_history[-1][1]
-    results = None
+    try:
+        client = AsyncClient()
+        message = chat_history[-1][1]
+        results = None
 
-    if web_search:
-        if agent:
-            response = await client.generate(
-                model=model,
-                prompt=message,
-                system="You are an AI Agent for writing Google queries. Write a clean query for the user to search the internet.",
-                think=False
-            )
-            message = response.response
-        results = search(message)
+        if web_search:
+            if agent:
+                response = await client.generate(
+                    model=model,
+                    prompt=message,
+                    system="You are an AI Agent for writing Google queries. Write a clean query for the user to search the internet.",
+                    think=False
+                )
+                message = response.response
+            results = search(message)
 
-    messages = []
-    if system_msg:
-        messages.append({"role": "system", "content": system_msg})
-    
-    messages += [{"role": r, "content": c} for r,c in chat_history]
-    messages.append({"role": "user", "content": message})
-    
-    if results:
-        messages.append({"role": "user", "type": "search", "content": f"Search results: {results}"})
-    
-    async for chunk in await client.chat(
-        model=model,
-        messages=messages,
-        stream=True,
-        keep_alive=0
-    ):
-        message = chunk.message.content
-        yield message
-        update_assistant_message(chat_id, message)
+        messages = []
+        if system_msg:
+            messages.append({"role": "system", "content": system_msg})
+        
+        messages += [{"role": r, "content": c} for r,c in chat_history]
+        
+        if results:
+            messages.append({"role": "user", "type": "search", "content": f"Search results: {results}"})
+        
+        async for chunk in await client.chat(
+            model=model,
+            messages=messages,
+            stream=True,
+        ):
+            message = chunk.message.content
+            yield message
+            update_assistant_message(chat_id, message)
+
+    except Exception as e:
+        yield f"ERROR: {e}"
+        update_assistant_message(chat_id, f"ERROR: {e}")
